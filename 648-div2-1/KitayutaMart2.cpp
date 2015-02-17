a236f4f99d72d4eb6a3af40b8ab175838770caf8
iwagaki <iwagaki@users.sourceforge.net>
2015-02-18 02:23:02 +0900

    Added 648-div2-1

---
+++ 648-div2-1/KitayutaMart2.cpp
@@ -0,0 +1,53 @@
+// BEGIN CUT HERE
+// END CUT HERE
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <math.h>
+
+using namespace std;
+
+class KitayutaMart2 {
+public:
+    int numBought(int K, int T) {
+        int sum = 0;
+        int num = 0;
+        for (;;) {
+            int price = K * pow(2, num);
+            if (sum + price > T)
+                break;
+
+            sum += price;
+            num++;
+        }
+        return num;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arg0 = 100; int Arg1 = 100; int Arg2 = 1; verify_case(0, Arg2, numBought(Arg0, Arg1)); }
+	void test_case_1() { int Arg0 = 100; int Arg1 = 300; int Arg2 = 2; verify_case(1, Arg2, numBought(Arg0, Arg1)); }
+	void test_case_2() { int Arg0 = 150; int Arg1 = 1050; int Arg2 = 3; verify_case(2, Arg2, numBought(Arg0, Arg1)); }
+	void test_case_3() { int Arg0 = 160; int Arg1 = 163680; int Arg2 = 10; verify_case(3, Arg2, numBought(Arg0, Arg1)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE                                                                                                                                    
+int main() {
+    KitayutaMart2 testRunner;
+    testRunner.run_test(-1);
+
+    return 0;
+}
+// END CUT HERE
