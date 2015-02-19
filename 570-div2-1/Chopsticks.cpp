9a78bdb52dcf3fa0f1c07139d3dd9f6c3e42f8e2
iwagaki <iwagaki@users.sourceforge.net>
2015-02-20 00:51:29 +0900

    Added 570-div2-1

---
+++ 570-div2-1/Chopsticks.cpp
@@ -0,0 +1,59 @@
+// BEGIN CUT HERE
+// END CUT HERE
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+class Chopsticks {
+public:
+    int getmax(vector <int> length) {
+        int count = 0;
+
+        while (length.size() > 0) {
+            int len = length.back();
+            length.pop_back();
+
+            for (vector<int>::iterator it = length.begin(); it != length.end(); ++it) {
+                if (len == *it) {
+                    ++count;
+                    length.erase(it);
+                    break;
+                }
+            }
+        }
+        
+        return count;
+    }
+
+    
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { int Arr0[] = {5,5}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; verify_case(0, Arg1, getmax(Arg0)); }
+	void test_case_1() { int Arr0[] = {1,2,3,2,1,2,3,2,1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 4; verify_case(1, Arg1, getmax(Arg0)); }
+	void test_case_2() { int Arr0[] = {1}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(2, Arg1, getmax(Arg0)); }
+	void test_case_3() { int Arr0[] = {1,2,3,4,5,6,7,8,9}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; verify_case(3, Arg1, getmax(Arg0)); }
+	void test_case_4() { int Arr0[] = {35,35,35,50,16,30,10,10,35,50,16,16,16,30,50,30,16,35,50,30,10,50,50,16,16,
+10,35,50,50,50,16,35,35,30,35,10,50,10,50,50,16,30,35,10,10,30,10,10,16,35}; vector <int> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 24; verify_case(4, Arg1, getmax(Arg0)); }
+
+// END CUT HERE
+
+};
+
+// BEGIN CUT HERE                                                                                                                                    
+int main() {
+    Chopsticks testRunner;
+    testRunner.run_test(-1);
+
+    return 0;
+}
+// END CUT HERE
